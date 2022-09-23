5117d2d896896b28300c4c94cdd8e0a0938fba89
rubylo718 <rubylo718@gmail.com>
2022-09-23 14:41:00 +0800

    feat: get multiple currency rates. make the source info in a json file

--- controllers/getRate-controller.js
+++ controllers/getRate-controller.js
@@ -1,19 +1,21 @@
 const { By } = require('selenium-webdriver')
 
-// 台灣銀行牌告匯率網站
-const web = 'https://rate.bot.com.tw/xrt?Lang=zh-TW'
-// USD spot selling rate(即期賣出匯率)xpath
-const path = '//*[@id="ie11andabove"]/div/table/tbody/tr[1]/td[5]'
+const currencyPathList = require('../paths/currencyPathList.json')
+const website = currencyPathList.url
+const currencies = currencyPathList.currencies
 
 const getRate = {
   getRate: async (driver) => {
     try {
-      await driver.get(web)
+      await driver.get(website)
       await driver.sleep(1000)
-      const data = await driver.findElements(By.xpath(path))
-      for (const item of data) {
-        const text = await item.getText()
-        console.log(text)
+      for (const currency of currencies) {
+        const data = await driver.findElements(By.xpath(currency.xpath))
+        for (const item of data) {
+          const text = await item.getText()
+          const currencyName = currency.name
+          console.log(`${currencyName} 即期賣出匯率: ${text}`)
+        }
       }
     } catch (error) {
       console.log('Error!', error)
