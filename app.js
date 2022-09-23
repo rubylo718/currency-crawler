53e88423566829b30be705481db01103c390cfba
rubylo718 <rubylo718@gmail.com>
2022-09-23 11:51:04 +0800

    style: add eslint and do formatting

--- app.js
+++ app.js
@@ -6,31 +6,28 @@ const app = express()
 const port = process.env.PORT || 3000
 // 台灣銀行牌告匯率網站
 const web = 'https://rate.bot.com.tw/xrt?Lang=zh-TW'
-// USD spot selling rate(即期賣出匯率)xpath 
-const path = `//*[@id="ie11andabove"]/div/table/tbody/tr[1]/td[5]`
+// USD spot selling rate(即期賣出匯率)xpath
+const path = '//*[@id="ie11andabove"]/div/table/tbody/tr[1]/td[5]'
 
-async function getRate() {
-    let driver = await new Builder().forBrowser('chrome').build()
-    try {
-      await driver.get(web)
-      await driver.sleep(1000)
-    
-      const data = await driver.findElements(By.xpath(path))
-      for (let item of data) {
-        let text = await item.getText()
-        console.log(text)
-      }
-    } catch(error) {
-      console.log('Error!', error)
-    } finally {
-      await driver.quit()
+async function getRate () {
+  const driver = await new Builder().forBrowser('chrome').build()
+  try {
+    await driver.get(web)
+    await driver.sleep(1000)
+    const data = await driver.findElements(By.xpath(path))
+    for (const item of data) {
+      const text = await item.getText()
+      console.log(text)
     }
+  } catch (error) {
+    console.log('Error!', error)
+  } finally {
+    await driver.quit()
+  }
 }
 
 getRate()
 
-
 app.listen(port, () => {
   console.info(`App is running on port ${port}`)
 })
-
